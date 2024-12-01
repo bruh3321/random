cb62a8c26618cc9bb85fac87e525215339dc8a4c
yes <abdouuu>
2024-12-01 16:17:57 +0100

    web

--- yuh/src/App.vue
+++ yuh/src/App.vue
@@ -10,7 +10,11 @@
 
   <body class="container2">
     <div class="mainText">
-      <h1></h1>
+      <img class="stuff" src="./assets/sometimes.jpg" alt="picture of self"></img>
+      <h3 class="stuff">pluh</h3>
+    </div>
+    <div class="actualbody" >
+      <h1 class>qualities/qualification</h1>
     </div>
   </body>
   <footer class="container3">
@@ -19,13 +23,35 @@
 </template>
 
 <style>
-.container1,
-.container2 {
+.container1 {
   display: flex;
   justify-content: center;
   align-items: center;
   box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
   transition: transform 0.3s;
   border-radius: 50px;
+}
+
+.container2,
+.mainText {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
+  transition: transform 0.3s;
+  border-radius: 50px;
+  margin-top: 30px;
+  margin-left: 575px;
+  margin-right: 550px;
+
+}
+
+.stuff {
+  margin-left: 50px;
+}
+
+img {
+  border-radius: 15px;
+  width: 100px;
 }
 </style>
--- yuh/vite.config.js
+++ yuh/vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // This allows access from other devices on the network
+    port: 3000, // You can specify the port if needed
+  },
 })

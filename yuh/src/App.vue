e0ff1c7fcdbd0321d49a64a7fb37f71989a75f1d
yes <abdouuu>
2024-12-01 09:35:55 +0100

    web

---
+++ yuh/src/App.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+
+</script>
+
+<template>
+
+  <div class="container1">
+    <h1 class="title">Test CV</h1>
+  </div>
+
+  <body class="container2">
+    <div class="mainText">
+      <h1></h1>
+    </div>
+  </body>
+  <footer class="container3">
+
+  </footer>
+</template>
+
+<style>
+.container1,
+.container2 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
+  transition: transform 0.3s;
+  border-radius: 50px;
+}
+</style>
